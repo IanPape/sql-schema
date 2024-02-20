e266cf6b9c78fc77a3de0dc8a1ae286130e30499
IanPape <141691712+IanPape@users.noreply.github.com>
2024-02-20 10:49:10 -0500

    Update air_traffic.sql

--- air_traffic.sql
+++ air_traffic.sql
@@ -1,6 +1,3 @@
--- from the terminal run:
--- psql < air_traffic.sql
-
 DROP DATABASE IF EXISTS air_traffic;
 
 CREATE DATABASE air_traffic;
@@ -13,13 +10,17 @@ CREATE TABLE travelers (
   last_name TEXT NOT NULL
 );
 
+CREATE TABLE seats (
+  id SERIAL PRIMARY KEY,
+  seat_number TEXT NOT NULL,
+  flight_id INTEGER REFERENCES flights(id)
+);
+
 CREATE TABLE flights (
   id SERIAL PRIMARY KEY,
-  seat TEXT NOT NULL,
   departure TIMESTAMP NOT NULL,
   arrival TIMESTAMP NOT NULL,
-  airline TEXT NOT NULL,
-  traveler_id INTEGER REFERENCES travelers(id)
+  airline TEXT NOT NULL
 );
 
 CREATE TABLE TravelersFlights (
@@ -28,13 +29,33 @@ CREATE TABLE TravelersFlights (
   PRIMARY KEY (traveler_id, flight_id)
 );
 
+CREATE TABLE TravelersSeats (
+  traveler_id INTEGER REFERENCES travelers(id),
+  seat_id INTEGER REFERENCES seats(id),
+  PRIMARY KEY (traveler_id, seat_id)
+);
+
+
 INSERT INTO travelers (first_name, last_name)
 VALUES ('Jennifer', 'Finch'), ('Thadeus', 'Gathercoal'), ('Sonja', 'Pauley'), ('Berkie', 'Wycliff'),
        ('Alvin', 'Leathes'), ('Cory', 'Squibbes');
 
-INSERT INTO flights (seat, departure, arrival, airline, traveler_id)
+INSERT INTO flights (departure, arrival, airline)
+VALUES ('2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United');
+
+INSERT INTO seats (seat_number, flight_id)
+VALUES ('33B', 1);
+
+INSERT INTO TravelersFlights (traveler_id, flight_id)
 VALUES
-  ('33B', '2018-04-08 09:00:00', '2018-04-08 12:00:00', 'United', 1);
+  (1, 1), (2, 1), (3, 1), (1, 1), (3, 1), (3, 1), (4, 1), (5, 1), (4, 1), (6, 1)
+ON CONFLICT (traveler_id, flight_id) DO NOTHING;
+
+INSERT INTO TravelersSeats (traveler_id, seat_id)
+VALUES
+  (1, 1), (2, 1), (3, 1), (1, 1), (3, 1), (3, 1), (4, 1), (5, 1), (4, 1), (6, 1)
+ON CONFLICT (traveler_id, seat_id) DO NOTHING;
+
 
 INSERT INTO TravelersFlights (traveler_id, flight_id)
 VALUES
